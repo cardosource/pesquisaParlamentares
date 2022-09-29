441d94818227ace6997485e876313f05cf7aae06
cardoso <jefersoncardoso.dev@gmail.com>
2022-09-29 00:06:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+var pesquisa1 = function(url){
+    return new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            let ajax = new XMLHttpRequest();
+            ajax.open('GET', url);
+            ajax.send(null);
+    
+            ajax.onreadystatechange = function(){
+                if(ajax.readyState === 4){
+                    if(ajax.status === 200){
+                        resolve(JSON.parse(ajax.responseText));
+                    }else{
+                        reject('Não foi encontrato nenhum usuário com este nome.')
+                    }
+                }
+            }
+
+        },1000);;//timeout
+    });
+}
+
+
+
 $(".informacao").css("height", "100px");
 $(".finalidade").fadeToggle();
 
